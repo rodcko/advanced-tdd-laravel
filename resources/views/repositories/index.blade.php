95201cd311f1baa280c99630f28c36f26aefb78a
Rodrigo Hernández <42217144+rodcko@users.noreply.github.com>
2021-05-12 00:11:56 -0600

    Final improvements

--- resources/views/repositories/index.blade.php
+++ resources/views/repositories/index.blade.php
@@ -7,12 +7,22 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <p class="text-right mb-4">
+          <a 
+            href="{{ route('repositories.create') }}" 
+            class="bg-blue-500 text-white font-bold py-2 px-4 rounded-md text-xs"
+          >
+            Agregar un nuevo repositorio
+          </a>
+        </p>
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
                 <table>
                   <thead>
                     <tr>
                       <th>ID</th>
                       <th>Enlace</th>
+                      <th>&nbsp;</th>
+                      <th>&nbsp;</th>
                       <th>&nbsp;</th>
                     </tr>
                   </thead>
@@ -30,11 +40,19 @@
                         <a href="{{ route('repositories.edit', $repository) }}">
                           Editar
                         </a>
-                      </td>                      
+                      </td>
+                      <td class="px-4 py-2">
+                        <form action="{{ route('repositories.destroy', $repository) }}" method="POST">
+                          @csrf
+                          @method('DELETE')
+
+                          <input type="submit" value="Eliminar" class="px-4 rounded-md bg-red-500 text-white">
+                        </form>
+                      </td>                                              
                     </tr>
                     @empty
                     <tr>
-                      <td colspan="4" >No hay repositorios creados</td>
+                      <td colspan="5" >No hay repositorios creados</td>
                     </tr>                    
                     @endforelse
                   </tbody>
