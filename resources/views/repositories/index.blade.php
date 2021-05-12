6a2ed8dfcca3cb32900046e3f287173614e3b33a
Rodrigo Hernández <42217144+rodcko@users.noreply.github.com>
2021-05-11 22:52:44 -0600

    Edition form

--- resources/views/repositories/index.blade.php
+++ resources/views/repositories/index.blade.php
@@ -26,10 +26,15 @@
                           Ver
                         </a>
                       </td>
+                      <td class="px-4 py-2">
+                        <a href="{{ route('repositories.edit', $repository) }}">
+                          Editar
+                        </a>
+                      </td>                      
                     </tr>
                     @empty
                     <tr>
-                      <td colspan="3" >No hay repositorios creados</td>
+                      <td colspan="4" >No hay repositorios creados</td>
                     </tr>                    
                     @endforelse
                   </tbody>
